a575e44340cb9feb1b2388a3d185118c583c36d7
bernardoeder881 <bernrdoeder881@gmail.com>
2026-04-02 21:27:39 -0300

    envia as atividades

--- atividade01.py
+++ atividade01.py
@@ -6,9 +6,14 @@
 #-------------------------------------------------------------------------------------------------
 #Aumento de salário de 15%
 
-salario = float (input("\nDigite o salario: "))
-aumento = salario * 0.15
-novo_salario = salario + aumento
+# salario = float (input("\nDigite o salario: "))
+# aumento = salario * 0.15
+# novo_salario = salario + aumento
 
-print(f"\nO novo salário é de R${salario:.2f} + R$ {aumento:.2f} de aumento, totalizando R$ {novo_salario:.2f}")
+# print(f"\nO novo salário é de R${salario:.2f} + R$ {aumento:.2f} de aumento, totalizando R$ {novo_salario:.2f}")
 
+numero = float (input ("Digite um número"))
+dobro= numero * 2
+triplo = numero *3
+
+print(f"O número escolhido foi {numero}, seu dobro é {dobro} e seu triplo é {triplo}.")
---
+++ atividade02.py
@@ -0,0 +1,4 @@
+numero = float (input ("Digite um número"))
+dobro= numero * 2
+triplo = numero *3
+print(f"O número escolhido foi {numero}, seu dobro é {dobro} e seu triplo é {triplo}.")
