2b71198cdb85fc308bd8f2c330da870db7c311c9
bernardoeder881 <bernrdoeder881@gmail.com>
2026-04-02 21:03:08 -0300

    envia a aula 02 ao GITHUB

---
+++ atividade01.py
@@ -0,0 +1,14 @@
+# numero = float(input("Digite um número"))
+# antecessor = numero - 1
+# sucessor = numero + 1 
+# print(f'O número é {numero}, seu antecessor é {antecessor} e o seu sucessor é {sucessor}.')
+
+#-------------------------------------------------------------------------------------------------
+#Aumento de salário de 15%
+
+salario = float (input("\nDigite o salario: "))
+aumento = salario * 0.15
+novo_salario = salario + aumento
+
+print(f"\nO novo salário é de R${salario:.2f} + R$ {aumento:.2f} de aumento, totalizando R$ {novo_salario:.2f}")
+
---
+++ exemplo01.py
@@ -0,0 +1,38 @@
+
+# print("Atividade de fixação.")
+
+# nome = "João."
+# bairro = "Centro."
+# profissao = "Professor."
+
+# print(f"Nome: {nome}")
+# print(f"Bairro: {bairro}")
+# print(f"Profissão: {profissao}")
+
+# #Operadores
+# preco = 20
+# quantidade = 3
+# total = preco * quantidade
+
+# print(f"O total é de {total}")
+
+# #Exemplo de entrada de valores pelo usuário
+
+# nome = input("\n\nInforme o nome: ")
+# salario =  float(input("\n\nInforme o salário: "))
+# bonus = float(input("\n\nInforme o bonus: "))
+
+# total = salario + bonus
+
+# print(f"\n\n {nome} ganha R${salario:.2f} de salário, e vai ganhar R${bonus:.2f} de bônus neste mês, totalizando R${total:.2f}.")
+
+# preco_produto = float (input("Digite o preço: "))
+# quantidade = int (input( "Digite a quantidade: "))
+# total = preco_produto * quantidade
+# desconto = total * 0.1
+# total_final = total * 0.9
+
+# print(f"o preço final é de R${total:.2f} - R$ {desconto:.2f} de desconto que fica em R$ {total_final:.2f}")
+
+
+
