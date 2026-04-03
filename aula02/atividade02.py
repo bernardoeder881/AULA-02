f6408b7935290fff95d0cd107e121223b8124d66
bernardoeder881 <bernrdoeder881@gmail.com>
2026-04-02 21:33:38 -0300

    Atualiza as atividades 1,2 e 3.

--- aula02/atividade02.py
+++ aula02/atividade02.py
@@ -1,4 +1,4 @@
-numero = float (input ("Digite um número"))
+numero = float (input ("Digite um número: "))
 dobro= numero * 2
-triplo = numero *3
+triplo = numero * 3
 print(f"O número escolhido foi {numero}, seu dobro é {dobro} e seu triplo é {triplo}.")
